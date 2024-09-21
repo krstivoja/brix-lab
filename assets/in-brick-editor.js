0b0f5ac50148c0d49c34869bf7040cba1503362e
Marko Krstic <mailme@markokrstic.com>
2024-09-21 11:48:12 +0200

    ...

--- assets/in-brick-editor.js
+++ assets/in-brick-editor.js
@@ -1,6 +1,13 @@
 function initTest() {
     console.log('in-brick-editor.js loaded');
 
+    let menuItem = document.querySelector('#bricks-toolbar > ul.group-wrapper.left');
+    if (menuItem) {
+        menuItem.insertAdjacentHTML('beforeend', `
+            <li id="swk-paste" data-key="'swiss_paste'"><span class="label">Marko</span></li>
+        `);
+    }
+
     const brxBody = document.querySelector('.brx-body');
     if (!brxBody) {
         console.error('brx-body element not found');
@@ -11,7 +18,32 @@ function initTest() {
     setTimeout(() => {
         if (brxBody.__vue_app__) {
             const vueApp = brxBody.__vue_app__.config.globalProperties;
-            console.log(vueApp);
+            // console.log(vueApp);
+
+            // Log all global classes
+            const globalClasses = vueApp.$_state.globalClasses;
+            // console.log('Global Classes:', globalClasses);
+
+            // Create a mapping of category IDs to names
+            const categoryMap = {};
+            const categories = [
+                { id: 'bqcwew', name: 'marko' },
+                { id: 'fxpjug', name: 'kategorija' }
+            ];
+
+            categories.forEach(category => {
+                categoryMap[category.id] = category.name;
+            });
+
+            // Log all categories with correct names
+            const globalCategories = globalClasses.map(globalClass => {
+                return {
+                    name: globalClass.name,
+                    category: globalClass.category ? categoryMap[globalClass.category] : null
+                };
+            });
+
+            console.log('Global Categories:', globalCategories);
         } else {
             console.error('__vue_app__ is not defined on brx-body');
         }
